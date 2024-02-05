b72082523ab7792b0d9226107660097e88121f96
juanjo <juanjopalma03@gmail.com>
2024-02-05 17:59:09 +0100

    post and get methods are done

--- controllers/indexControllers.js
+++ controllers/indexControllers.js
@@ -1,72 +1,114 @@
 const connection = require("../config/db");
+const fetch = require('node-fetch');
 
 class indexControllers {
 
-  // obtengo los datos de la api (bien)
-  getDataCountryofAPI = (req, res) => {
+  // // obtengo los datos de la api (bien)
+  // getDataCountryofAPI = (req, res) => {
+  //   let url = "https://restcountries.com/v3.1/all?fields=name,population";
+
+  //   fetch(url)
+  //       .then((response) => response.json())
+  //       .then( (data) => { 
+  //         const datos = data?.map((country) => ({
+  //           name: country.name.common,
+  //           population: country.population
+  //         }));
+
+  //         res.json(datos);
+  //         console.log(res);
+  //       })
+  //       .catch((err) => console.log('Error al llamar a la API:', err))
+  // }
+
+  // Inserta todos los países de la api
+  postDataCountry = (req, res) => {
+
     let url = "https://restcountries.com/v3.1/all?fields=name,population";
 
     fetch(url)
         .then((response) => response.json())
         .then( (data) => { 
           const datos = data?.map((country, index) => ({
-            country_id: index + 1,
+            id: index + 1,
             name: country.name.common,
             population: country.population
           }));
 
-          res.json(datos);
-          console.log(res);
+          let sql = `SELECT * FROM country`; 
+
+          connection.query(sql, (err, results) => {
+            if (err) {
+              console.log("Error al verificar la existencia de datos en la bd", err);
+              return res.status(500).json({ error: "Error al verificar la existencia de datos en la bd" });
+            }
+
+            // me introduce los datos en la bd en caso de estar vacía dicha bd: bien
+            else if (results.length === 0) {
+              datos.forEach( ({ id, name, population }) => {
+                let sql2 = `INSERT INTO country (country_id, common_name, population) VALUES ('${id}', '${name}', ${population})`;
+          
+                connection.query(sql2, (err, result) => {
+                  if (err) {
+                    console.log("Error al introducir los datos en la bd", err);
+                  } 
+                })
+              })
+            }
+
+            // en caso de 
+            else if (results.length !== 0) {
+              console.log("Ya están los datos en la bd", err);
+              return res.status(500).json({ error: "Ya están los datos en la bd" });
+            }
+          })
+          
         })
         .catch((err) => console.log('Error al llamar a la API:', err))
+
   }
 
-  // almacenaje y actualizaciónde de los datos de la api
-  postDataCountry = (req, res) => {
-    const jsonData = req.body;
-    console.log(jsonData); // me trae los datos bien 
+  // obtener los países de la base de datos
+  getDataCountry = (req, res) => {
 
-    let sql = `SELECT * FROM country`; 
+    const country = req.params.country;
+    console.log(country);
+
+    let sql = `SELECT * FROM country WHERE common_name = '${country}'`; 
 
     connection.query(sql, (err, results) => {
       if (err) {
-        console.log("Error al verificar la existencia de datos en la bd", err);
-        return res.status(500).json({ error: "Error al verificar la existencia de datos en la bd" });
+        console.log("Error al traer el país de la bd", err);
+        return res.status(500).json({ error: "Error al traer los países de la bd" });
       }
-
-      // me introduce los datos en la bd en caso de estar vacía dicha bd: bien
       else if (results.length === 0) {
-        jsonData.forEach( ({ country_id, name, population }) => {
-          let sql2 = `INSERT INTO country (country_id, common_name, population) VALUES (${country_id},'${name}', ${population})`;
-    
-          connection.query(sql2, (err, result) => {
-            if (err) {
-              console.log("Error al introducir los datos en la bd", err);
-            } else {
-              console.log("datos introducidos en bd");
-            }
-          })
-        })
+        console.log("Ha escrito mal el país o el país no existe en la bd");
+        return res.status(400).json({ error: "Ha escrito mal el país o el país no existe en la bd" });
       }
-
-      // me actualiza la bd con los nuevos datos: bien
       else {
-        jsonData.forEach( ({country_id, name, population}) => {
-          let sql2 = `UPDATE country SET common_name = "${name}", population = ${population} WHERE country_id = ${country_id}`;
-    
-          connection.query(sql2, (err, result) => {
-            if (err) {
-              console.log("Error al actualizar los db", err);
-            } else {
-              console.log("datos actualizados en bd");
-            }
-          })
-        })
+        console.log("País obtenidos de la bd");
+        return res.status(200).json(results)
       }
     })
   }
 
+  // obtener los países de la base de datos
+  getDataCountries = (req, res) => {
+    let sql = `SELECT * FROM country`; 
+
+    connection.query(sql, (err, results) => {
+      if (err) {
+        console.log("Error al traer los países de la bd", err);
+        return res.status(500).json({ error: "Error al traer los países de la bd" });
+      }
+      else {
+        console.log("Datos obtenidos de la bd");
+        return res.status(200).json(results)
+      }
+    })
+  }
 
 }
+
 
 module.exports = new indexControllers;
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,15 @@ const indexControllers = require('../controllers/indexControllers');
 // http://localhost:3000/api/v1/data/country
 
 // endpoint para obtener los datos de la API
-router.get('/api/v1/data/country', indexControllers.getDataCountryofAPI);
+// router.get('/api/v1/data/country', indexControllers.getDataCountryofAPI);
 
-// endpoint para almacenar y actualizar los datos de la API
-router.post('/api/v1/data/country', indexControllers.postDataCountry);
+// endpoint para almacenar los datos de la API
+router.post('/api/v1/data/countries', indexControllers.postDataCountry);
 
-// endpoint para obtener de la bd los datos de la API
-// router.get('/api/v1/data/country', indexControllers.getDataOfDataBase);
+// endpoint para obtener un país de la bd
+router.get('/api/v1/data/:country', indexControllers.getDataCountry);
+
+// endpoint para obtener todos los países de la bd
+router.get('/api/v1/data/countries', indexControllers.getDataCountries);
 
 module.exports = router;
